992ae1a2086e2602816938037b1f5dadfccda13e
Lachezar Borisov <ly.borisov@gmail.com>
2020-04-19 10:32:39 +0300

    Updates

---
+++ precache-manifest.2e7e2b6e3c43bd010ee7a2d98c416813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcd901f1603514ee9f4b42c246377b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a68918d0cb1eb4d8ab0c",
+    "url": "/monsters-rolodex/static/css/main.6ff9d3cf.chunk.css"
+  },
+  {
+    "revision": "c17b3112af99f9a6368d",
+    "url": "/monsters-rolodex/static/js/2.187272e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.187272e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68918d0cb1eb4d8ab0c",
+    "url": "/monsters-rolodex/static/js/main.ceb6f9a8.chunk.js"
+  },
+  {
+    "revision": "1dcea1c53874fb11b132",
+    "url": "/monsters-rolodex/static/js/runtime-main.50f1aaa7.js"
+  }
+]);
